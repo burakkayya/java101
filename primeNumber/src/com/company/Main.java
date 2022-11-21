68b40f30be7945a217432fb8686f4a1102744174
burakkayya <56073855+burakkayya@users.noreply.github.com>
2022-11-21 18:45:10 +0300

    prime number until 100

---
+++ primeNumber/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 100; i++) {
+            int counter = 0;
+            for (int j = 2; j <= i; j++) {
+                if (i % j == 0) {
+                    counter++;
+
+                }
+            }
+            if (counter == 1) System.out.print(i + ",");
+        }
+
+    }
+}
