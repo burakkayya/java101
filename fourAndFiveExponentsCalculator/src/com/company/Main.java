9ef150e1335cfa5da004a13bffb557cbd30099a1
burakkayya <56073855+burakkayya@users.noreply.github.com>
2022-11-21 16:15:44 +0300

    four and five exponents calculator

---
+++ fourAndFiveExponentsCalculator/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int number=sc.nextInt();
+
+        System.out.print("Four exponents until "+number+" : ");
+        for (int i=1; i<=number; i*=4){
+            System.out.print(i + "   ");
+        }
+
+        System.out.println();
+        64
+        System.out.print("Five exponents until "+number+" : ");
+        for (int i=1; i<=number; i*=5){
+            System.out.print(i + "   ");
+        }
+    }
+}
